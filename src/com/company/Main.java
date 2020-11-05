b545d288d89316455c5f38db0a99f1d47ed5bff0
maxim760 <maks.tomanov@mail.ru>
2020-11-05 19:09:09 +0300

    initial

---
+++ src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car bmw = new Car();
+//        bmw.doMove();
+        bmw.power= 500;
+        bmw.name = "BMW";
+        bmw.setRuntime(120);
+//        System.out.println(bmw.getRuntime());
+
+        ECar tesla = new ECar("tesla",200,13,122,100);
+//        tesla.doMove();
+        tesla.doFuel(22);
+        System.out.println(tesla);
+    }
+}
